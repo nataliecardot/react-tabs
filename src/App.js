6e64768ec18544be405d3bc823fc13af5657af91
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-17 16:10:25 -0700

    Fetch jobs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
 
 const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
   return <h2>tabs project setup</h2>;
 }
 
