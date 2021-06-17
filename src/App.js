1966931b58fd0dd2caec2fcdbbb71caf1b9ac991
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-17 16:33:02 -0700

    Display single job info item

--- src/App.js
+++ src/App.js
@@ -19,7 +19,37 @@ function App() {
     fetchJobs();
   }, []);
 
-  return <h2>tabs project setup</h2>;
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty, index) => {
+          return (
+            <div key={index} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{duty}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default App;
