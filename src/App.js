5842bc802aa98552f97b9389ea9810c55c0caaf5
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-17 16:48:43 -0700

    Render button for each job

--- src/App.js
+++ src/App.js
@@ -35,18 +35,25 @@ function App() {
         <h2>Experience</h2>
         <div className="underline"></div>
       </div>
-      <div className="job-info">
-        <h3>{title}</h3>
-        <h4>{company}</h4>
-        <p className="job-date">{dates}</p>
-        {duties.map((duty, index) => {
-          return (
-            <div key={index} className="job-desc">
-              <FaAngleDoubleRight className="job-icon" />
-              <p>{duty}</p>
-            </div>
-          );
-        })}
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((item, index) => (
+            <button key={item.id}>{item.company}</button>
+          ))}
+        </div>
+        <div className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
